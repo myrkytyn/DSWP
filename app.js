adf3b561a3890bc6ee31a837b32fb032ecab1502
Maksym <makcm007@gmail.com>
2022-03-22 23:25:03 +0200

    regexp added

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
-    const navItem = document.querySelectorAll('.nav-item')
+    const navItem = document.querySelector('.nav-item');
 
     burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
@@ -19,7 +19,41 @@ const navSlide = () => {
     });
 }
 
-navSlide();
+let subscr = false;
+const helpButton = () => {
+    const button = document.querySelector('.helpbtn');
+    const errorElement = document.querySelector('.error');
+    const inputAmount = document.querySelector('.amount');
+    const form = document.querySelector('.form')
+
+    button.addEventListener('click', (e) => {
+        let message = '';
+        let reg = /^\d+$/;
+        if (!reg.test(inputAmount.value)) {
+            message = 'Введіть тільки цифри, будь ласка';
+
+        }
+        if (inputAmount.value === '' || inputAmount.value == null) {
+            message = 'Введіть бажану суму допомоги притулку';
+        }
+        if (inputAmount.value.length > 6) {
+            message = 'Введіть суму меншу за 999 999'
+        }
+        if (message !== '') {
+            e.preventDefault()
+            errorElement.innerText = message
+        } else {
+            errorElement.innerText = '';
+            if (!subscr) {
+                pay(inputAmount.value);
+            } else {
+                subscription(inputAmount.value);
+            }
+        }
+    })
+
+}
+
 
 const switchPayment = () => {
     const oneTime = document.querySelector('.onetime')
@@ -27,17 +61,19 @@ const switchPayment = () => {
     const helpbtn = document.querySelector('.helpbtn')
 
     oneTime.addEventListener('click', () => {
-        oneTime.classList.toggle('switch-active');
+        oneTime.classList.add('switch-active');
         subscription.classList.remove('switch-active');
-        helpbtn.setAttribute("onClick", "pay(this.form.custom_amount.value);")
+        subscr = false;
     });
     subscription.addEventListener('click', () => {
-        subscription.classList.toggle('switch-active');
+        subscription.classList.add('switch-active');
         oneTime.classList.remove('switch-active');
-        helpbtn.setAttribute("onClick", "subscription(this.form.custom_amount.value);")
+        subscr = true;
     });
 
 
 }
 
 switchPayment();
+navSlide();
+helpButton();
