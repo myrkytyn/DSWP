3d99a8069d726d573a9166354cebd0987bd40114
Maksym <makcm007@gmail.com>
2022-03-23 12:31:09 +0200

    deleting responsive menu and burger

--- app.js
+++ app.js
@@ -1,24 +1,3 @@
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navItem = document.querySelectorAll('.nav-item');
-
-    burger.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
-
-        navItem.forEach((item, index) => {
-            if (item.style.animation) {
-                item.style.animation = ''
-            } else {
-                item.style.animation = `navLinksFade 0.5s ease forwards ${index / 7 + 0.3}s`;
-            }
-        });
-
-        //Burger Animation
-        burger.classList.toggle('toggle')
-    });
-}
-
 let subscr = false;
 const helpButton = () => {
     const button = document.querySelector('.helpbtn');
@@ -75,5 +54,4 @@ const switchPayment = () => {
 }
 
 switchPayment();
-navSlide();
 helpButton();
